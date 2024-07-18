67edc46f609afd8cbebd7d6bfc09c27095475d77
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-07-18 14:59:51 +0200

    evaluation grid can be read & submitted

--- components/forms/evaluationGrid.tsx
+++ components/forms/evaluationGrid.tsx
@@ -4,7 +4,6 @@ import EvaluationAxis from "./evaluationAxis";
 import { EvalAxis, EvaluationState, EvaluationDB } from "./interfaces";
 import { createClient } from "@/utils/supabase/client";
 
-
 export default function EvaluationGrid({
   evalAxes,
   data,
@@ -14,27 +13,27 @@ export default function EvaluationGrid({
 }) {
   const initialState: EvaluationState = {
     identify: [
-      { score: data && data.identify_problem_def, comment: "" },
-      { score: 0, comment: "" },
+      { score: data && data.identify_problem_def, comment: data?data.identify_problem_comment:"" },
+      { score: data && data.identify_evidence_of_research, comment: data?data.identify_evidence_of_research_comment:"" },
     ],
     design: [
-      { score: 0, comment: "" },
-      { score: 0, comment: "" },
+      { score: data && data.design_project_plan, comment: data?data.design_project_plan_comment:"" },
+      { score: data && data.design_all_involved, comment: data?data.design_all_involved_comment:"" },
     ],
     create: [
-      { score: 0, comment: "" },
-      { score: 0, comment: "" },
+      { score: data && data.create_clear_model, comment: data?data.create_clear_model_comment:"" },
+      { score: data && data.create_explanation_of_innovation, comment: data?data.create_explanation_of_innovation_comment:"" },
     ],
     iterate: [
-      { score: 0, comment: "" },
-      { score: 0, comment: "" },
+      { score: data && data.iterate_sharing_of_solution, comment: data?data.iterate_sharing_of_solution_coment:"" },
+      { score: data && data.iterate_evidence_improvement, comment: data?data.iterate_evidence_improvement_comment:"" },
     ],
     communicate: [
-      { score: 0, comment: "" },
-      { score: 0, comment: "" },
+      { score: data && data.communicate_explanation_clarity, comment: data?data.communicate_explanation_clarity_comment:"" },
+      { score: data && data.communicate_pride, comment: data?data.communicate_pride_comment:"" },
     ],
-    global_positive_feedback: "",
-    global_negative_feedback: "",
+    globalPositiveFeedback: data?data.global_positive_feedback:"",
+    globalNegativeFeedback: data?data.global_negative_feedback:"",
   };
 
   const [evaluation, setEvaluation] = useState<EvaluationState>(initialState);
@@ -43,19 +42,30 @@ export default function EvaluationGrid({
     e.preventDefault();
 
     const updatedEvaluation = {
-      identify_problem_def :evaluation.identify[0].score
-    }
+      identify_problem_def: evaluation.identify[0].score,
+      identify_evidence_of_research: evaluation.identify[1].score,
+
+      design_project_plan: evaluation.design[0].score,
+      design_all_involved: evaluation.design[1].score,
+
+      create_clear_model: evaluation.create[0].score,
+      create_explanation_of_innovation: evaluation.create[1].score,
+
+      iterate_sharing_of_solution: evaluation.iterate[0].score,
+      iterate_evidence_improvement: evaluation.iterate[1].score,
+
+      communicate_explanation_clarity: evaluation.communicate[0].score,
+      communicate_pride: evaluation.communicate[1].score,
+    };
 
     const supabase = createClient();
 
     const { error } = await supabase
       .from("innovation_scoresheet")
       .update(updatedEvaluation)
-      .eq('team_id',1);
+      .eq("team_id", 1);
 
-    console.log(error?.code)
-
-    const response = {};
+    console.log(error?.code);
   };
 
   return (

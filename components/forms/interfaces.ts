64598ba4255831fac08857565aa6995c7966c988
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-07-19 12:17:55 +0200

    added a new modal in case the form isn't fully completed && added the persistence of all comments in database

--- components/forms/interfaces.ts
+++ components/forms/interfaces.ts
@@ -9,7 +9,7 @@ export interface EvalAxis {
 
 export interface EvaluationItem {
   score: number | null;
-  comment: string | null;
+  comment: string;
 }
 
 // this is the interface for the evaluation state, inside of the form
@@ -19,8 +19,9 @@ export interface EvaluationState {
   create: EvaluationItem[];
   iterate: EvaluationItem[];
   communicate: EvaluationItem[];
-  globalPositiveFeedback: string | null;
-  globalNegativeFeedback: string | null;
+  globalPositiveFeedback: string;
+  globalNegativeFeedback: string;
+  judgePrivateNotes: string;
 }
 
 // this is the interface for the data that is fetched from the db
@@ -29,25 +30,26 @@ export interface EvaluationDB {
   judging_session_id: number;
   team_id: number;
   identify_problem_def: number | null;
-  identify_problem_comment: string | null;
+  identify_problem_def_comment: string;
   identify_evidence_of_research: number | null;
-  identify_evidence_of_research_comment: string | null;
+  identify_evidence_of_research_comment: string;
   design_project_plan: number | null;
-  design_project_plan_comment: string | null;
+  design_project_plan_comment: string;
   design_all_involved: number | null;
-  design_all_involved_comment: string | null;
+  design_all_involved_comment: string;
   create_clear_model: number | null;
-  create_clear_model_comment: string | null;
+  create_clear_model_comment: string;
   create_explanation_of_innovation: number | null;
-  create_explanation_of_innovation_comment: string | null;
+  create_explanation_of_innovation_comment: string;
   iterate_sharing_of_solution: number | null;
-  iterate_sharing_of_solution_coment: string | null;
+  iterate_sharing_of_solution_comment: string;
   iterate_evidence_improvement: number | null;
-  iterate_evidence_improvement_comment: string | null;
+  iterate_evidence_improvement_comment: string;
   communicate_explanation_clarity: number | null;
-  communicate_explanation_clarity_comment: string | null;
+  communicate_explanation_clarity_comment: string;
   communicate_pride: number | null;
-  communicate_pride_comment: string | null;
-  global_positive_feedback: string | null;
-  global_negative_feedback: string | null;
+  communicate_pride_comment: string;
+  global_positive_feedback: string;
+  global_negative_feedback: string;
+  judge_private_notes: string;
 }
